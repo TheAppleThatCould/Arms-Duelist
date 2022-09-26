71e42d84cd85fe75ae720a99b9060eeef50adfc2
Joshua Adams <joshuaadams031999@gmail.com>
2022-09-26 10:27:53 +1000

    Change the colour of the background for the screens

--- Assets/Arms Duelist/Scripts/MainMenu.cs
+++ Assets/Arms Duelist/Scripts/MainMenu.cs
@@ -7,9 +7,8 @@ public class MainMenu : MonoBehaviour
 {
 
 
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    public void LoadLevel (string levelName) {
+        SceneManager.LoadScene(levelName);
     }
 
     public void QuitGame()
