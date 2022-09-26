b835eaee9e3a4c5f5c57953419a015f603d92c71
Joshua Adams <joshuaadams031999@gmail.com>
2022-09-26 12:04:13 +1000

    create gun scripts to shoot a pistol

---
+++ Assets/Arms Duelist/Scripts/Weapon Scripts/Gun.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private GunData gunData;
+    [SerializeField] private Transform muzzle;
+
+    float timeSinceLastShot;
+
+    private void Start(){
+        PlayerShoot.shootInput += Shoot;
+
+        PlayerShoot.reloadInput += StartReload;
+    }
+
+
+    public void StartReload(){
+        if(!gunData.reloading){
+            StartCoroutine(Reload());
+        }
+    }
+
+    private IEnumerator Reload(){
+        gunData.reloading = true;
+        yield return new WaitForSeconds(gunData.reloadTime);
+
+        gunData.currentAmmo = gunData.magSize;
+
+        gunData.reloading = false;
+    }
+
+    // Check to see if the player is reloading or is in the process of shooting their gun
+    private bool CanShoot() => !gunData.reloading && timeSinceLastShot > 1f / (gunData.fireRate / 60f);
+
+    public void Shoot(){
+        // Check to see if player has ammo
+        if(gunData.currentAmmo > 0){
+            if(CanShoot()){
+                if(Physics.Raycast(muzzle.position, muzzle.forward, out RaycastHit hitInfo, gunData.maxDistance)){
+                    // Debug.Log(hitInfo.transform.name);
+                    IDamageable damageable = hitInfo.transform.GetComponent<IDamageable>();
+                    damageable?.TakeDamage(gunData.damage);
+                }
+
+                gunData.currentAmmo--;
+                timeSinceLastShot = 0;
+                OnGunShot();
+            }
+        }
+    }
+
+    private void Update(){
+        timeSinceLastShot += Time.deltaTime;
+
+        Debug.DrawRay(muzzle.position, muzzle.forward);
+    }
+
+    private void OnGunShot(){
+        // throw new NotImplmentedExcepection();
+    }
+}
