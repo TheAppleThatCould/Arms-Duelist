e335838095ebac84f965bf207a1ac76bfebba5f3
jet <jx2421533662@gmail.com>
2022-10-02 22:53:44 +1000

    naming update

--- Assets/SoulBoss.cs
+++ Assets/SoulBoss.cs
@@ -44,7 +44,7 @@ public class SoulBoss : MonoBehaviour
 
 
         float distance = Vector3.Distance(player.transform.position, transform.position);
-        if (distance <= fitDistance)//����
+        if (distance <= fitDistance)
         {
             navAi.isStopped = true;
 
