f456d4951a3d701965b653f6f9384d78d81abaf5
jet <jx2421533662@gmail.com>
2022-10-08 14:55:47 +1000

    fixed restart game not reset the enemies

--- Assets/Scripts/newPlayer.cs
+++ Assets/Scripts/newPlayer.cs
@@ -8,7 +8,7 @@ using UnityStandardAssets.Characters.FirstPerson;
 public class newPlayer : MonoBehaviour
 {
     // Get the first person controller for the purpose of unlocking the mouse lock.
-    FirstPersonController firstPersonController;
+    private FirstPersonController firstPersonController;
 
     public float HP = 100;
     private float MaxHP;
@@ -35,11 +35,10 @@ public class newPlayer : MonoBehaviour
     {
         Fire();
         // Start to decrement the timer for power up duration.
-        if(powerUpTimer >= 0){
+        if (powerUpTimer >= 0)
+        {
             powerUpTimer -= Time.deltaTime;
-            if(powerUpTimer < 1){
-                unlimitedAmmo = false;
-            }
+            if (powerUpTimer < 1) unlimitedAmmo = false;
         }
     }
 
@@ -56,55 +55,56 @@ public class newPlayer : MonoBehaviour
 
     public void Fire()
     {
-        if (Input.GetMouseButtonDown(0)&& BulletNum > 0 && isEquiped == true )
-        {   
+        if (Input.GetMouseButtonDown(0) && BulletNum > 0 && isEquiped == true)
+        {
             fx1.Play();
             fx2.Play();
             source.PlayOneShot(shootclip);
-            
+
             // Check if the player currently have unlimited ammo power up
-            if(!unlimitedAmmo){
-                BulletNum--;
-            }
+            if (!unlimitedAmmo) BulletNum--;
 
-            bulletText.text = "Bullet:" + BulletNum.ToString() + "/"+ totalBulletNum.ToString();
+            bulletText.text = "Bullet:" + BulletNum.ToString() + "/" + totalBulletNum.ToString();
 
-            Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width/2,Screen.height/2));
+            var ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
             RaycastHit hitinfo;
 
-            if (Physics.Raycast(ray, out hitinfo)&&hitinfo.collider.tag=="enemy")
-            {   
+            if (Physics.Raycast(ray, out hitinfo) && hitinfo.collider.tag == "enemy")
                 hitinfo.collider.GetComponent<SoulBoss>().TakeDamage(damage);
-            }
-
         }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
             // If the ammo is more the the total magazine then completely reload the gun else just fill up the gun with the remaining ammo.
-            if(ammo >= totalBulletNum){
+            if (ammo >= totalBulletNum)
+            {
                 // Decrement the amount of ammo it takes to reload the gun
                 ammo -= totalBulletNum;
                 // Asign the totalBullet the gun can hold to the bullet number
                 BulletNum = totalBulletNum;
-            } else if(ammo > 0){
+            }
+            else if (ammo > 0)
+            {
                 BulletNum = BulletNum + ammo;
                 ammo = 0;
             }
 
             bulletText.text = "Bullet:" + BulletNum.ToString() + "/" + totalBulletNum.ToString();
-        } else {
+        }
+        else
+        {
             // Debug.Log("Out of ammo");
         }
     }
 
     public Slider hpslider;
+
     public void TakeDamage(float damage)
     {
         if (HP <= 0) return;
 
         HP -= damage;
-        if (HP<=0)
+        if (HP <= 0)
         {
             HP = 0;
             Time.timeScale = 0;
@@ -114,6 +114,7 @@ public class newPlayer : MonoBehaviour
             firstPersonController = gameObject.GetComponent<FirstPersonController>();
             firstPersonController.unlockMouseLock();
             SceneManager.LoadScene("LoseScene");
+            Time.timeScale = 0;
         }
 
         hpslider.value = HP / MaxHP;
@@ -126,21 +127,24 @@ public class newPlayer : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-	// Increment ammo when player picks up ammo
-	public void ApplyAmmoPickup() {
+    // Increment ammo when player picks up ammo
+    public void ApplyAmmoPickup()
+    {
         source.PlayOneShot(pickUpSound);
-		ammo = ammo + 20;
-	}
+        ammo = ammo + 20;
+    }
 
     // Increment health when player picks up health
-	public void ApplyHealthPickup() {
+    public void ApplyHealthPickup()
+    {
         source.PlayOneShot(pickUpSound);
-		HP = HP + 20;
+        HP = HP + 20;
         hpslider.value = HP / MaxHP;
-	}
+    }
 
     // A function that will set the bulletNum to -1 inorder to let the player have unlimited ammo
-    public void ApplyUnlimitedAmmoPickup(){
+    public void ApplyUnlimitedAmmoPickup()
+    {
         // Increment the ammo by 1 to be fair.
         BulletNum = BulletNum + 1;
         // Set the power up timer
